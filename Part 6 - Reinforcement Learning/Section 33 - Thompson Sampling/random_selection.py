4f8e58f90a36616e9d745c821c25da7125d558e8
Prashant Jha <prashant.pkj01@gmail.com>
2018-07-10 13:22:01 +0530

    part 6 commit 2

--- Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/random_selection.py
+++ Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/random_selection.py
@@ -1,16 +1,14 @@
-#Upper confidene bound
+# Random Selection
 
-#importing the libraries
-
+# Importing the libraries
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
-#importing dataset
+# Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
-#Implementing Random selection
-
+# Implementing Random Selection
 import random
 N = 10000
 d = 10
@@ -22,9 +20,9 @@ for n in range(0, N):
     reward = dataset.values[n, ad]
     total_reward = total_reward + reward
 
-#visualising the results - Histogram
+# Visualising the results
 plt.hist(ads_selected)
 plt.title('Histogram of ads selections')
 plt.xlabel('Ads')
 plt.ylabel('Number of times each ad was selected')
-plt.show() 
+plt.show()
